ef0a1a83ec1c3cc94e0721537dc88096888e1452
U-MIILITS\tarvi <miilits@gmail.com>
2017-04-22 12:32:34 +0300

    script

--- kt/kt8.php
+++ kt/kt8.php
@@ -7,76 +7,28 @@
   <title>Kodutöö nr8</title>
   <meta name="description" content="Tarvi Kodutööd">
   <meta name="author" content="Tarvi Tihhanov">
-    <style>
-    
-        form {
-            
-            border-style: groove;
-            border-radius: 5px;
-            clear: both;
-            padding: 10px;
-            margin-left: 600px;
-            margin-right: 250px;
-            font-family:monospace;
-            }
-    
-        input, select{
-            
-            margin-bottom: 20px;
-        }
-        
-        input[type=submit]{
-            
-            
-        }
-        
-        #valjund {
-            
-            margin: auto;
-            padding: 10px;
-            background-color: <?php echo $_GET["taustav2rv"]; ?>;
-            color: <?php echo $_GET["tekstiv2rv"]; ?>;
-            border-width: <?php echo $_GET["piirjoon"]; ?>;
-            border-style: <?php echo $_GET["piirstiil"];?>;
-            border-radius: <?php echo $_GET["piirnurk"].'px';?>;
-            border-color: <?php echo $_GET["piirv2rv"]; ?>;
-            font-family:monospace;
-            width: 650px;
-            height: 350px;
-          
-        }
-    
-    </style>
+<link rel="stylesheet" href="../css/kt8.css">
 </head>
 
-    <body>
-    
-
- 
+<body>
 <p id="valjund">
-            
-          
- <?php  
-           function tekstBoks(){ 
-            
+                  
+<?php  
+function tekstBoks(){  
      if (isset($_GET['auktekstile']) && $_GET['auktekstile']!="") {
-        echo htmlspecialchars ($_GET['auktekstile']);}
-             
+        echo htmlspecialchars ($_GET['auktekstile']);}      
       }
-            tekstBoks();    
-            
+tekstBoks();          
 ?>
-</p>
-        
- 
-        
-<p>
-        
+</p>   
+
+<p>    
 <form action="kt8.php" method="get" id="vorm">
     <textarea name="auktekstile"  form="vorm" placeholder="Trüki siia"></textarea><br>
     <input type="color" name="taustav2rv" value="#36040E"> Taustavärvus<br>
     <input type="color" name="tekstiv2rv" value="#15DFD6"> Tekstivärvus<br>
-</p>    
+</p>   
+ 
 <p>
 <b>Piirjoon: </b><br>
                 <select name="piirjoon" form="vorm"> 
@@ -96,9 +48,9 @@
                     <option value="solid">Solid</option>
                 </select>
                     
-                <input type="color" name="piirv2rv" value="#11FA06"> Piirjoone värvus<br>
-                <input type="number" name="piirnurk" min="0" max="100" step="1"> Piirjoone nurga raadius (0-100px)<br>
-                <input type="submit" value="Esita">
+    <input type="color" name="piirv2rv" value="#11FA06"> Piirjoone värvus<br>
+    <input type="number" name="piirnurk" min="0" max="100" step="1"> Piirjoone nurga raadius (0-100px)<br>
+    <input type="submit" value="Esita">
                       
 </p>
 </form> 
