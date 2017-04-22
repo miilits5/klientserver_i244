ded83dc1eb72988b9d849c19a1b5cda82abb7921
U-MIILITS\tarvi <miilits@gmail.com>
2017-04-22 13:13:32 +0300

    script

--- kt/multipage/vote.php
+++ kt/multipage/vote.php
@@ -4,19 +4,19 @@ require_once('head.html');
 <div id="wrap">
 	<h3>Vali oma lemmik :)</h3>
 	<form action="tulemus.php" method="GET">
-		<p>
+		
 <?php
 $pildid = range(1,6);
 	foreach($pildid as $value){
 		echo "<p>
 		<label for='p$value'>
-				<img src='pildid/nameless$value.jpg' alt='nimetu $value' height='50'/>
+				<img src='pildid/nameless$value.jpg' alt='nimetu $value' height='75'/>
 				</label>
 				<input type='radio' value='$value' id='p$value' name='pilt'/>
 			</p>";
 	}
 ?>
-		</p>
+		
 		<br/>
 		<input type="submit" value="Valin!"/>
 	</form>
