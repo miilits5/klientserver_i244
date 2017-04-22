72854403358c1cfd5d87bcfc74cc84e7b45d2925
U-MIILITS\tarvi <miilits@gmail.com>
2017-04-22 12:39:34 +0300

    script

---
+++ kt/kt8_2.php
@@ -0,0 +1,31 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>Kodutöö nr8</title>
+  <meta name="description" content="Tarvi Kodutööd">
+  <meta name="author" content="Tarvi Tihhanov">
+
+</head>
+
+<body >
+    <?php
+$dir = "kt"; // kausta nimi, mida avada
+$failid = array(); // massiiv, kuhu lisatakse leitud failid
+if ($dh = opendir($dir)) { // kui funktsioon opendir vastava sisendiga õnnestub, siis jäta viide kaustale meelde muutujasse $dh ning läbi järgnev koodiblokk
+  while (($file = readdir($dh)) !== false) { // seni, kuni funktsiooniga readdir vastavas kaustas saab kätte mingi kirje (fail/kaust), salvesta see kirje muutujasse $file ning läbi järgnev koodiblokk
+    if(!is_dir($file)) { // juhul, kui saadud kirje ei ole kaust, siis lisa antud kirje failide massiivi
+      $failid[] = $file;
+    }
+  }
+  closedir($dh); // kui kausta lugemine on läbi, sulge ühendus kaustaga.
+} else { // kui funktsioon opendir luhtub(kaust puudub), siis esita veateade ja lõpeta programmi töö
+  die("Ei suuda avada kataloogi $dir");
+}
+print_r($failid);// kuva failide massiivi sisu
+?>
+    
+</body>
+</html>
