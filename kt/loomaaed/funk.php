699ecccff63c950f6ee115f0a39f3b13671d2413
U-MIILITS\tarvi <miilits@gmail.com>
2017-05-02 18:39:59 +0300

    script2

--- kt/loomaaed/funk.php
+++ kt/loomaaed/funk.php
@@ -38,9 +38,9 @@ function kuva_puurid(){
 	$puurinr = array();
 	$puurid = array();
 	
-	while ($row = mysqli_fetch_assoc($tulemus), ){
+	while ($row = mysqli_fetch_assoc($tulemus)){
 		$puurinr[] = $row;
-		while ($row2 =  mysqli_fetch_assoc($tulemus2), ){
+		while ($row2 =  mysqli_fetch_assoc($tulemus2)){
 		$puurid[$puurinr][] = $row2;
 		
 	}
