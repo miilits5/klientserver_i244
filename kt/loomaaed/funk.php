e6e32970939d40de548cfe925bdf84bb3159b4be
U-MIILITS\tarvi <miilits@gmail.com>
2017-05-02 19:21:33 +0300

    script2

--- kt/loomaaed/funk.php
+++ kt/loomaaed/funk.php
@@ -30,7 +30,7 @@ function kuva_puurid(){
 	$connection = mysqli_connect($host, $user, $pass, $db) or die("ei saa ühendust mootoriga- ".mysqli_error());
 	
 	$kysimine = "SELECT DISTINCT(puur) FROM ttihhano10142660_loomaaed";
-	$nimed = "SELECT * FROM ttihhano10142660_loomaaed where puur = ?";
+	$nimed = "SELECT * FROM ttihhano10142660_loomaaed where puur = "$puurinr"";
 	$tulemus = mysqli_query($connection, $kysimine);
 	$tulemus2 = mysqli_query($connection, $nimed);
 
@@ -48,7 +48,7 @@ function kuva_puurid(){
 	
 	print_r($puurinr);
 	print_r($puurid);
-	
+	print_r($puurid[$puurinr][]);
 
 	include_once('views/puurid.html');
 	
